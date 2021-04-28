7a917eaa908203eaba579c1bd3dc79a4a062e27d
R2D2 <jp@Joses-MBP.attlocal.net>
2021-04-27 23:54:48 -0700

    added inital server with passport Oauth, need to add mongoose

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
+
+const keys = require("./config/keys");
+const authRoutes = require("./routes/authRoutes");
+
+const passport = require("passport");
+
+// require("./models/User");
+require("./services/passport");
+
+
 const app = express();
 
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 /// first route!
 app.get("/", (res, req) => {
 	req.send("<h1>hello world</h1>");
 });
-
 
 // Local and heroku posts
 const PORT = process.env.PORT || 5000;
